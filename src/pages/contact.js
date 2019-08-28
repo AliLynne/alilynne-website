0b675c9c0aacb74fdd3bfdb87df57ba88268a399
AliLynne <alilynne.thompson@gmail.com>
2019-08-28 13:00:26 -0700

    contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faDev } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faDev, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import Layout from '../components/layout'
 import Head from '../components/head'
@@ -13,6 +14,7 @@ const ContactPage = () => {
     <Layout>
       <Head title="Contact" />
       <h1>Contact Info</h1>
+      <p>Feel free to contact me whichever way works for you!</p>
       <ul className={contactStyles.list}>
         <li className={contactStyles.list__item}>
           <a className={contactStyles.list__icon} href="https://www.twitter.com/alilynnet">
@@ -22,6 +24,21 @@ const ContactPage = () => {
         <li className={contactStyles.list__item}>
           <a className={contactStyles.list__icon} href="https://dev.to/alilynne">
             <FontAwesomeIcon icon={faDev} />
+          </a>
+        </li>
+        <li className={contactStyles.list__item}>
+          <a className={contactStyles.list__icon} href="https://www.github.com/alilynne">
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </a>
+        </li>
+        <li className={contactStyles.list__item}>
+          <a className={contactStyles.list__icon} href="https://www.linkedin.com/in/alilynne/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+        <li className={contactStyles.list__item}>
+          <a className={contactStyles.list__icon} href="mailto:alilynne.thompson@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
           </a>
         </li>
       </ul>

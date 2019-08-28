e57b9e49a2d4b6576027fc17102c2d74dcafa1a4
AliLynne <alilynne.thompson@gmail.com>
2019-08-28 13:31:33 -0700

    adjusted footer

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,8 +9,18 @@ const AboutPage = () => {
       <Layout>
         <Head title="About" />
         <h1>About</h1>
-        <p>Bio details will go here.</p>
-        <Link to="/contact">Contact me here.</Link>
+        <p>'m a full stack web developer currently contributing to f/oss. Mu current focus is on React and Gatsby, but I love all things JavaScript.</p>
+        <p>Currently open to Full-Time or Part-Time Web Development in the San Francisco Bay Area or Remote.</p>
+        <ul>
+          <li>
+            <Link to="/projects">Check out my projects</Link>
+          </li>
+          <li> 
+            <Link to="/contact">Contact me</Link>
+          </li>
+        </ul>
+        
+        
       </Layout>
   )
 }

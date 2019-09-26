6042a03d26629ee3b334501ff7d15fb461e5e27c
AliLynne <alilynne.thompson@gmail.com>
2019-09-26 10:39:50 -0700

    set up circleci - fix bug in tag template

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const Tags = ({ pageContext, data }) => {
